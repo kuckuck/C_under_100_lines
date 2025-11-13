961e0e6acdb7ad3fa2a0cd34221c98e8c76528ca
kuckuck <nicholaskuck@gmail.com>
2025-11-13 08:35:49 -0800

    ascii_table

---
+++ ascii_table/ascii.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    unsigned char count;
+    for(count=32;count<255; count++){
+        printf(" %3d - %c",count,count);
+        if(count % 6==0)
+            printf("\n");
+    }
+    printf("\n");
+    return 0;
+
+}
